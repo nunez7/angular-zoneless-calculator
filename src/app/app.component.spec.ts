194d38aebb47a045583f8fe623c4905f94fb45c6
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-17 10:09:32 -0700

    Configuraciones para testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'zoneless-calculator' title`, () => {
+  /*it(`should have the 'zoneless-calculator' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('zoneless-calculator');
@@ -25,5 +25,5 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, zoneless-calculator');
-  });
+  });*/
 });
