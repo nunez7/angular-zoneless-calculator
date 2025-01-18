0362caa595faeb74e4b34a0becfcca96f1b311b9
Felix Nunez <felix.nunez@vass.com.mx>
2025-01-17 18:00:50 -0700

    Simulando acciones de keypress

--- src/app/calculator/components/calculator/calculator.component.spec.ts
+++ src/app/calculator/components/calculator/calculator.component.spec.ts
@@ -82,4 +82,25 @@ describe('CalculatorComponent', () => {
         expect(buttons[2].textContent?.trim()).toBe('%');
         expect(buttons[3].textContent?.trim()).toBe('÷');
       });
+
+      it('Should handle keyboard events correctly', () => {
+        const eventEnter = new KeyboardEvent('keyup', { key: 'Enter' });
+        document.dispatchEvent(eventEnter);
+        expect(mockCalculatorService.constructNumber).toHaveBeenCalledWith('=');
+    
+        const eventESC = new KeyboardEvent('keyup', { key: 'Escape' });
+        document.dispatchEvent(eventESC);
+        expect(mockCalculatorService.constructNumber).toHaveBeenCalledWith('C');
+      });
+
+      it('Should display result text correctly', () => {
+        mockCalculatorService.resultText.and.returnValue('123');
+        mockCalculatorService.subResultText.and.returnValue('10');
+        mockCalculatorService.lastOperator.and.returnValue('-');
+        fixture.detectChanges();
+    
+        expect(component.resultText()).toBe('123');
+    
+        expect(compiled.querySelector('#subResult')?.textContent).toContain('10 -');
+      });
 });  
